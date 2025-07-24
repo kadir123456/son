c7bae295c7fed6483fdd730e96769ab7a372b840
kadir123456 <47676558+kadir123456@users.noreply.github.com>
2025-07-24 11:35:10 +0300

    Update trading_strategy.py

--- app/trading_strategy.py
+++ app/trading_strategy.py
@@ -18,14 +18,12 @@ class TradingStrategy:
         latest_candle = klines[-1]
 
         # Mum verilerini Binance API formatına göre alıyoruz:
-        # [..., 'volume', ..., 'taker_buy_base_asset_volume', ...]
-        #   5. index -> Toplam Hacim
-        #   9. index -> Alıcıların Başlattığı İşlem Hacmi (Taker Buy Volume)
+        # 5. index -> Toplam Hacim
+        # 9. index -> Alıcıların Başlattığı İşlem Hacmi (Taker Buy Volume)
         
         total_volume = float(latest_candle[5])
         taker_buy_volume = float(latest_candle[9])
 
-        # Eğer toplam hacim sıfırsa bir şey yapma
         if total_volume == 0:
             return "HOLD"
             
